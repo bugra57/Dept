58b968dcdf5736d4d4850a76d4740d62b9322ea1
bugra57 <bugra.karaaslan@hva.nl>
2022-02-06 14:00:48 +0100

    work section added

---
+++ src/data/WorkData.js
@@ -0,0 +1,74 @@
+import React from 'react';
+import bolcom from '../assets/images/bolcom.png';
+import chocomel from '../assets/images/chocomel.png';
+import gemeenteMuseum from '../assets/images/gemeentemuseum.png';
+import jbl from '../assets/images/jbl.png';
+import kempen from '../assets/images/kempen.png';
+import koninklijkeBibliotheek from '../assets/images/koninklijke-bibliotheek.png';
+import philips from '../assets/images/philips.png';
+import tui from '../assets/images/tui.png';
+import zalando from '../assets/images/zalando.png';
+import libertyGlobal from '../assets/images/liberty-global.png';
+
+export const WorkData = [
+    {
+        id: 1,
+        name: "BOL.COM",
+        title: "A summer island in the Netherlands",
+        image: bolcom
+    },
+    {
+        id: 2,
+        name: "KEMPEN",
+        title: "Not some average banking website",
+        image: kempen
+    },
+    {
+        id: 3,
+        name: "PHILIPS",
+        title: "Beautiful design meets innovative technology",
+        image: philips
+    },
+    {
+        id: 4,
+        name: "GEMEENTEMUSEUM",
+        title: "A 100 years of Mondriaan & De stijl",
+        image: gemeenteMuseum
+    },
+    {
+        id: 5,
+        name: "BE LIGHTING",
+        title: "Delivering clarity on a lobal scale",
+        image: libertyGlobal
+    },
+    {
+        id: 6,
+        name: "TUI",
+        title: "Swipe to find your next holiday destination",
+        image: tui
+    },
+    {
+        id: 7,
+        name: "CHOCOMEL",
+        title: "A campaign for the limited editon letter packs",
+        image: chocomel
+    }, 
+    {
+        id: 8,
+        name: "JBL",
+        title: "Live like a champion with Jerome Boateng",
+        image: jbl
+    },
+    {
+        id: 9,
+        name: "ZALANDO",
+        title: "Innovative SEO and content strategy for Zalando",
+        image: zalando
+    },
+    {
+        id: 10,
+        name: "KONINKLIJKE BIBLIOTHEEK",
+        title: "The search for the most influential book ever",
+        image: koninklijkeBibliotheek
+    }, 
+];
--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,15 +1,46 @@
 import React from 'react';
 import backgroundIMG from '../assets/images/florensis.png';
 import '../assets/styles/Homepage.css';
+import { WorkData } from '../data/WorkData';
+import * as IoIcon from 'react-icons/io'
+import { IconContext } from "react-icons/lib";
 
 function Homepage() {
-    
 
     return (
         <div className='homepage'>
-           <header className='header'>
-               <img src={backgroundIMG} alt="background image" className='bgImage'/>
-           </header>
+            <header className='header'>
+                <img src={backgroundIMG} alt="background image" className='bgImage' />
+            </header>
+
+            <div className='filterSection'>
+
+            </div>
+
+            <div className='workSection'>
+                <div className='workContainer'>
+                    {
+                        WorkData.map((data, idx) => {
+                            return (
+                                <div className='card'>
+                                    <img src={data.image} alt="image" className='workImg'></img>
+                                    <p className='nameTxt'>{data.name}</p>
+                                    <p className='titleTxt'>{data.title}</p>
+                                    <div className='link'>
+                                        <IconContext.Provider value={{ color: "#1A18F7", size: "30px" }}>
+                                            <div>
+                                                <IoIcon.IoMdArrowDropright />
+                                            </div>
+                                        </IconContext.Provider>
+                                        <p className='linkTxt'>VIEW CASE </p>
+                                    </div>
+
+                                </div>
+                            )
+                        })
+                    }
+                </div>
+            </div>
         </div>
     )
 }
