518bb8550265802d07c87fc8271e16c14ca9e71b
bugra57 <bugra.karaaslan@hva.nl>
2022-02-06 17:38:55 +0100

    client section is done and it is repsonsive

---
+++ src/data/ClientsData.js
@@ -0,0 +1,100 @@
+import React from 'react';
+import abn from '../assets/client-logo/ABN_logo_zwart.png';
+import adidas from '../assets/client-logo/Adidas_logo_zwart.png';
+import klm from '../assets/client-logo/KLM_logo_zwart.png';
+import microsoft from '../assets/client-logo/Microsoft_logo_zwart-320x161.png';
+import mona from '../assets/client-logo/Mona_logo_zwart.png';
+import nivea from '../assets/client-logo/Nivea_logo_zwart.png';
+import nn from '../assets/client-logo/NN_logo_zwart.png';
+import Oxxio from '../assets/client-logo/Oxxio_logo_zwart.png';
+import tomtom from '../assets/client-logo/Tomtom_logo_zwart.png';
+import transavia from '../assets/client-logo/Transavia_logo_zwart.png';
+import triumph from '../assets/client-logo/Triumph_logo_zwart.png';
+import unilever from '../assets/client-logo/Unilever_logo_zwart.png';
+import walibi from '../assets/client-logo/Walibi_logo_zwart.png';
+import Zalando from '../assets/client-logo/Zalando_logo_zwart.png';
+import ziggo from '../assets/client-logo/Ziggo_logo_zwart.png';
+import pathe from '../assets/client-logo/Pathe_logo_zwart.png';
+
+export const ClientsData = [
+    {
+        id: 1,
+        name: "Nivea",
+        image: nivea
+    },
+    {
+        id: 2,
+        name: "Mona",
+        image: mona 
+    },
+    {
+        id: 3,
+        name: "Transavia",
+        image: transavia
+    },
+    {
+        id: 4,
+        name: "Zalando",
+        image: Zalando 
+    },
+    {
+        id: 5,
+        name: "Tomtom",
+        image: tomtom
+    },
+    {
+        id: 6,
+        name: "Unilever",
+        image: unilever 
+    },
+    {
+        id: 7,
+        name: "Adidas",
+        image: adidas 
+    },
+    {
+        id: 8,
+        name: "Pathe",
+        image: pathe 
+    },
+    {
+        id: 9,
+        name: "ABN-AMRO",
+        image: abn 
+    },
+    {
+        id: 10,
+        name: "Triumph",
+        image: triumph 
+    },
+    {
+        id: 11,
+        name: "Microsoft",
+        image: microsoft 
+    },
+    {
+        id: 12,
+        name: "Oxxio",
+        image: Oxxio 
+    },
+    {
+        id: 13,
+        name: "Nationale Nederlanden",
+        image: nn 
+    },
+    {
+        id: 14,
+        name: "Ziggo",
+        image: ziggo 
+    },
+    {
+        id: 15,
+        name: "Walibi",
+        image: walibi 
+    },
+    {
+        id: 16,
+        name: "KLM",
+        image: klm 
+    },
+]
--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import backgroundIMG from '../assets/images/florensis.png';
 import '../assets/styles/Homepage.css';
-import { WorkData } from '../data/WorkData';
 import * as IoIcon from 'react-icons/io'
 import { IconContext } from "react-icons/lib";
+
+// Data import 
+import { WorkData } from '../data/WorkData';
+import { ClientsData } from '../data/ClientsData';
+
 
 function Homepage() {
 
@@ -20,9 +24,9 @@ function Homepage() {
             <div className='workSection'>
                 <div className='workContainer'>
                     {
-                        WorkData.map((data, idx) => {
+                        WorkData.map((data) => {
                             return (
-                                <div className='card'>
+                                <div className='card' key={data.id}>
                                     <img src={data.image} alt="image" className='workImg'></img>
                                     <p className='nameTxt'>{data.name}</p>
                                     <p className='titleTxt'>{data.title}</p>
@@ -47,6 +51,15 @@ function Homepage() {
                 <p className='clientTxt'>
                     We value a great working relationship with our clients above all else. It's why then often come to our parties. It's also why we're able to challengen and inspire them to reach for the stars.
                 </p>
+                <div className='clientContainer'>
+                    {
+                        ClientsData.map((data) => {
+                            return(
+                                <img src={data.image} alt="clientImage" key={data.id} className="clientImage"></img>
+                            )
+                        })
+                    }
+                </div>
             </div>
         </div>
     )
